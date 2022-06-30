52549794d98d55e53824432c08acc77e99d03b5b
Daniel <Danielmicah14/mse19-dmicah@poly.ac.mw>
2022-06-30 05:05:06 -0700

    commiting task 4 once again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio>
-/**This is a simple program in c
- * and it will prints
- * the statement:
- * Programming is like building a multilingual puzzle
- * using puts function instead of printf function
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	/*the puts function*/
 	puts("\"Programming is like building a multilingual puzzle");
-	
-	/*ending the program with the value 0*/
 	return (0);
 }
