7975fbaffc1ca3aff697729a792cb6baf9efa7dc
Daniel <Danielmicah14/mse19-dmicah@poly.ac.mw>
2022-06-30 04:15:54 -0700

    commiting the answers to all the first 7 tasks from zero to 6

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,12 @@
+#include <stdio>
+/**writing a simple program
+ * to display the text:
+ * With proper grammar, but the outcome is a piece of art
+ * using printf function
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+
+	return (0);
+}
