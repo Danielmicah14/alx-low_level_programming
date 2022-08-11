3e092292256486ff420d78098309d85a9a17017e
Daniel Micah <mse19-dmicah@poly.ac.mw>
2022-08-11 22:48:14 +0200

    commiting task 4 once again

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,21 +1,19 @@
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
+
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */ int main(void) {
-    list_t *head;
-    head = NULL;
-    add_node_end(&head, "Jennie");
-    add_node_end(&head, "&");
-    add_node_end(&head, "Jay");
-    add_node_end(&head, "love");
-    add_node_end(&head, "asm");
-    print_list(head);
-    free_list(head);
-    head = NULL;
-    return (0);
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
 }
